bd9463c62cff73a0f37cd5b8a44861ac6cf6af84
paolodalex <paolodalex96@gmail.com>
2022-06-11 15:11:37 +0200

    feat: add paragraph to app

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import {Text} from "test-1-dalex/atoms/text/Text";
-
+import { Paragraph } from "test-1-dalex/molecules/paragraph/Paragraph";
 const App = () => ( 
     <div>
+        <Paragraph />
     </div>
 );
 
