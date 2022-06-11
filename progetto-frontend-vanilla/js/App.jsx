fc9210429fa886e5c3a677d96a999a0c6e54304b
paolodalex <paolodalex96@gmail.com>
2022-06-11 16:51:00 +0200

    fix: add title on app

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Text } from "test-1-dalex/atoms/text/Text";
 import { Paragraph } from "test-1-dalex/molecules/paragraph/Paragraph";
+import { Title } from "test-1-dalex/molecules/title/Title";
 const App = () => ( 
     <div>
+        <Title children={<Text text ="questo è un titolo"/>}/>
         <Paragraph children={<Text text="provaa" />} />
         <Text text="ciao"/>
     </div>
