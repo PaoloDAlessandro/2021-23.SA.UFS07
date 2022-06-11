9ddac6e73fa29d0780cbf9d948f7a0a8753c6daa
paolodalex <paolodalex96@gmail.com>
2022-06-11 14:46:21 +0200

    fix: removed the title import from app

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {Text} from "test-1-dalex/atoms/text/Text";
-import { Title } from "../../design-system/react/src/molecules/title/Title";
 
 const App = () => ( 
     <div>
