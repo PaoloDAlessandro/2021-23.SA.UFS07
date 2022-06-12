fb1d19a2b9c7fa6a9add267f2d177734a4e7f6d3
paolodalex <paolodalex96@gmail.com>
2022-06-12 17:27:05 +0200

    feat: create title molecule

--- design-system/react/src/molecules/title/Title.jsx
+++ design-system/react/src/molecules/title/Title.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import "../../fonts/stylesheet.css"
+import "./title.css";
 /**
  * Primary UI component for user interaction
  */
-export const Title = ({children}) => {
+export const Title = ({children, width_size}) => {
 
   return (
-    <h1>
+    <h1 style={{width:width_size}}>
         {children}
     </h1>
   );
 
 };
 
-Title.PropTypes = {
+Title.propTypes = {
+  width_size: PropTypes.string,
 }
