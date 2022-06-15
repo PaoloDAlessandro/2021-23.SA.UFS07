5a51ca4027c1f9e4a9833f64767939895d328a80
paolodalex <paolodalex96@gmail.com>
2022-06-15 17:37:57 +0200

    fix: change marca style

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -16,6 +16,7 @@ import { Card_title } from 'test-1-dalex/umd/atoms/card_title/Card_title';
 import { Card_image_container } from "test-1-dalex/umd/molecules/card_image_container/Card_image_container";
 import { Card_image } from "test-1-dalex/umd/atoms/card_image/Card_image";
 import { Card } from "test-1-dalex/umd/organisms/card/Card";
+import { Cards_container } from "test-1-dalex/umd/organisms/cards_container/Cards_container";
 import ImagePeso from '../img/peso.svg';
 import ImageTimer from '../img/timer.svg';
 import ImageQuantità from '../img/quantità.svg';
@@ -25,6 +26,10 @@ import ImagePantry from '../img/pantry.svg';
 import ImageRecipes from '../img/recipes.svg';
 import ImageAccount from '../img/account.svg';
 import ImageCremaCarciofi from '../img/cremaCarciofi.png';
+import ImageRaguContadino from '../img/raguContadino.png';
+import ImageLeerdammer from '../img/leerdammer.png';
+import ImageRosette from '../img/rosette.png';
+
 
 
 const App = () => ( 
@@ -50,6 +55,7 @@ const App = () => (
           </>
           }></Header> 
 
+          <Cards_container>
 
           <Card>
 
@@ -90,6 +96,207 @@ const App = () => (
           </Card_data>
           </>
           </Card>
+
+          <Card>
+
+          <>
+          <Card_image_container back_color={"#AA3C24"}>
+            <Card_image src = {ImageRaguContadino}/>
+          </Card_image_container>
+
+
+          <Card_data>
+          <>
+          <Card_title text={"Ragù contadino"}></Card_title>
+          <Card_marca text={"Barilla"} class_style={"marca"}></Card_marca>
+
+          <Card_infos>
+
+          <>
+              <Card_single_info> 
+                  <Image src= {ImagePeso} id={"peso"} />
+                  <Card_marca text={"500 g"} class_style={"card_text"}></Card_marca>
+              </Card_single_info>
+          </>
+
+          <>
+          <Card_single_info> 
+              <Image src= {ImageQuantità} id={"quantità"} />
+              <Card_marca text={"1"} class_style={"card_text"}></Card_marca>
+          </Card_single_info>
+          </>
+          <>
+          <Card_single_info> 
+              <Image src= {ImageTimer} id={"timer"} />
+              <Card_marca text={"Pronto"} class_style={"card_text"}></Card_marca>
+          </Card_single_info>
+          </>
+          </Card_infos>
+          </>
+          </Card_data>
+          </>
+          </Card>
+
+          <Card>
+
+          <>
+          <Card_image_container back_color={"#EFC516"}>
+            <Card_image src = {ImageLeerdammer}/>
+          </Card_image_container>
+
+
+          <Card_data>
+          <>
+          <Card_title text={"Leerdammer"}></Card_title>
+          <Card_marca text={"Leerdammer"} class_style={"marca"}></Card_marca>
+
+          <Card_infos>
+
+          <>
+              <Card_single_info> 
+                  <Image src= {ImagePeso} id={"peso"} />
+                  <Card_marca text={"350 g"} class_style={"card_text"}></Card_marca>
+              </Card_single_info>
+          </>
+
+          <>
+          <Card_single_info> 
+              <Image src= {ImageQuantità} id={"quantità"} />
+              <Card_marca text={"4"} class_style={"card_text"}></Card_marca>
+          </Card_single_info>
+          </>
+          <>
+          <Card_single_info> 
+              <Image src= {ImageTimer} id={"timer"} />
+              <Card_marca text={"Pronto"} class_style={"card_text"}></Card_marca>
+          </Card_single_info>
+          </>
+          </Card_infos>
+          </>
+          </Card_data>
+          </>
+          </Card>
+          <Card>
+
+          <>
+          <Card_image_container back_color={"#E8D6BB"}>
+            <Card_image src = {ImageRosette}/>
+          </Card_image_container>
+
+
+          <Card_data>
+          <>
+          <Card_title text={"Rosette"}></Card_title>
+          <Card_marca text={"Schär"} class_style={"marca"}></Card_marca>
+
+          <Card_infos>
+
+          <>
+              <Card_single_info> 
+                  <Image src= {ImagePeso} id={"peso"} />
+                  <Card_marca text={"500 g"} class_style={"card_text"}></Card_marca>
+              </Card_single_info>
+          </>
+
+          <>
+          <Card_single_info> 
+              <Image src= {ImageQuantità} id={"quantità"} />
+              <Card_marca text={"5"} class_style={"card_text"}></Card_marca>
+          </Card_single_info>
+          </>
+          <>
+          <Card_single_info> 
+              <Image src= {ImageTimer} id={"timer"} />
+              <Card_marca text={"8 min."} class_style={"card_text"}></Card_marca>
+          </Card_single_info>
+          </>
+          </Card_infos>
+          </>
+          </Card_data>
+          </>
+          </Card>
+
+          <Card>
+
+            <>
+            <Card_image_container back_color={"#418C43"}>
+              <Card_image src = {ImageCremaCarciofi}/>
+            </Card_image_container>
+
+
+            <Card_data>
+            <>
+            <Card_title text={"Crema carciofi"}></Card_title>
+            <Card_marca text={"Knorr"} class_style={"marca"}></Card_marca>
+
+            <Card_infos>
+
+            <>
+                <Card_single_info> 
+                    <Image src= {ImagePeso} id={"peso"} />
+                    <Card_marca text={"250 g"} class_style={"card_text"}></Card_marca>
+                </Card_single_info>
+            </>
+
+            <>
+            <Card_single_info> 
+                <Image src= {ImageQuantità} id={"quantità"} />
+                <Card_marca text={"2"} class_style={"card_text"}></Card_marca>
+            </Card_single_info>
+            </>
+            <>
+            <Card_single_info> 
+                <Image src= {ImageTimer} id={"timer"} />
+                <Card_marca text={"25 min."} class_style={"card_text"}></Card_marca>
+            </Card_single_info>
+            </>
+            </Card_infos>
+            </>
+            </Card_data>
+            </>
+            </Card>
+
+            <Card>
+
+            <>
+            <Card_image_container back_color={"#418C43"}>
+              <Card_image src = {ImageCremaCarciofi}/>
+            </Card_image_container>
+
+
+            <Card_data>
+            <>
+            <Card_title text={"Crema carciofi"}></Card_title>
+            <Card_marca text={"Knorr"} class_style={"marca"}></Card_marca>
+
+            <Card_infos>
+
+            <>
+                <Card_single_info> 
+                    <Image src= {ImagePeso} id={"peso"} />
+                    <Card_marca text={"250 g"} class_style={"card_text"}></Card_marca>
+                </Card_single_info>
+            </>
+
+            <>
+            <Card_single_info> 
+                <Image src= {ImageQuantità} id={"quantità"} />
+                <Card_marca text={"2"} class_style={"card_text"}></Card_marca>
+            </Card_single_info>
+            </>
+            <>
+            <Card_single_info> 
+                <Image src= {ImageTimer} id={"timer"} />
+                <Card_marca text={"25 min."} class_style={"card_text"}></Card_marca>
+            </Card_single_info>
+            </>
+            </Card_infos>
+            </>
+            </Card_data>
+            </>
+            </Card>
+
+            </Cards_container>
 
 
           <Footer children={
