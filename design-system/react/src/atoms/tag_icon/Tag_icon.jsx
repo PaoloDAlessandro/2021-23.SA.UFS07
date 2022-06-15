4d153584fb0199e77cdf2d11fcb63cafcc255cd7
paolodalex <paolodalex96@gmail.com>
2022-06-15 18:59:16 +0200

    feat: created tag_icon atom

---
+++ design-system/react/src/atoms/tag_icon/Tag_icon.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import "./tag_icon.css"
+/**
+ * Primary UI component for user interaction
+ */
+export const Tag_icon = ({children}) => {
+
+  return (
+    <div className='tag-icon'>
+        {children}
+    </div>
+  );
+
+};
+
+Tag_icon.propTypes = {
+    
+}
