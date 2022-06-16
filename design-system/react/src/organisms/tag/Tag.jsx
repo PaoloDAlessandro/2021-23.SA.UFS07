7569cce8b2168109bc84ab74695c628369a07d51
paolodalex <paolodalex96@gmail.com>
2022-06-17 00:10:14 +0200

    fix: tag id

--- design-system/react/src/organisms/tag/Tag.jsx
+++ design-system/react/src/organisms/tag/Tag.jsx
@@ -4,15 +4,15 @@ import "./tag.css"
 /**
  * Primary UI component for user interaction
  */
-export const Tag = ({children, back_color}) => {
+export const Tag = ({children, back_color, id}) => {
 
   return (
-    <div className='tag' style={{backgroundColor:back_color}}>
+    <div className='tag' id={id} style={{backgroundColor:back_color}}>
         {children}
     </div>
   );
 };
 
 Tag.propTypes = {
-    
+    id: PropTypes.string,
 }
