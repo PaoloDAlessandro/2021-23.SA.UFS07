97eb098cdecf9e07b7e9029107ae1fc16c6e9c53
paolodalex <paolodalex96@gmail.com>
2022-06-22 20:15:53 +0200

    feat: add new tailwind css

--- modern-css-with-tailwind/src/index.js
+++ modern-css-with-tailwind/src/index.js
@@ -19,6 +19,25 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+window.addEventListener("scroll", function(){
+  var screenY = window.pageYOffset || document.documentElement.scrollTop;
+  const developer = this.document.getElementById("developer");
+  const marketing = this.document.getElementById("marketing");
+  var init = 50
+  var maxX = (window.innerWidth * 0.8 * 0.8) - 300;
+  const multi = 6;
+  var positionX = init + (screenY * multi);
+  console.log(positionX);
+  console.log("maxX: " + maxX);
+  if (positionX > maxX) {
+    console.log(maxX);
+    positionX  = maxX;
+  }
+  developer.style.left = positionX + "px";
+  marketing.style.left = positionX + "px";
+});
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
