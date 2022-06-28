c8a4281c2d6afd8050081b7d57d5d7ac0fcf7dd8
paolodalex <paolodalex96@gmail.com>
2022-06-28 12:55:15 +0200

    feat: add tailwind to design system

--- design-system/react/src/atoms/button/Button.jsx
+++ design-system/react/src/atoms/button/Button.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
+
 /**
  * Primary UI component for user interaction
  */
 export const Button = ({ label, onClick, type }) => {
   return (
     <button
+      tw="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"
       onClick={onClick}
       type={type}
     >
